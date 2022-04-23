72bd06e86af74e0d34fadf09c33b1eb019a7199e
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-04-23 23:10:24 +0900

    fix: neonの仕様変更に伴った修正

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,20 @@
-import { app, BrowserWindow, ipcMain, dialog, Menu, session } from 'electron';
-import path from 'path';
-import * as isDev from 'electron-is-dev';
-import fs from 'fs';
-import { electron } from 'webpack';
+import { app, BrowserWindow, ipcMain, dialog, Menu, session } from "electron";
+import path from "path";
+import * as isDev from "electron-is-dev";
+import fs from "fs";
+import { electron } from "webpack";
 
 // TODO Priority:middle デバックビルドとリリースビルドをソースコードの変更なく実行できるようにする．現段階では不十分．
 if (isDev) {
-    require('electron-reload')(path.join(process.cwd(), 'build'), {
-        electron: path.join(
-            process.cwd(),
-            'node_modules',
-            '.bin',
-            'electron.cmd',
-        ),
-    });
+	require("electron-reload")(
+		path.join(process.cwd(), "build"),
+		{
+			electron: path.join(process.cwd(), "node_modules", ".bin", "electron.cmd"),
+		},
+	);
 }
 
-const native = require('../native');
+const native = require("../index.node");
 // const musicPlayer = new native.MusicPlayer(
 //     'C:/Users/Yudai/workspace/Project/Last-Music/01 Fight oh! MIRAI oh!.flac',
 // );
@@ -25,54 +23,68 @@ const native = require('../native');
 // 参考: https://qiita.com/pochman/items/64b34e9827866664d436
 let mainWindow: BrowserWindow | null = null;
 const createWindow = (): void => {
-    // レンダープロセスとなる、ウィンドウオブジェクトを作成する。
-    mainWindow = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        webPreferences: {
-            nodeIntegration: false,
-            nodeIntegrationInWorker: false,
-            contextIsolation: true,
-            preload: path.join(__dirname, 'preload.js'),
-            devTools: isDev,
-        },
-    });
+	// レンダープロセスとなる、ウィンドウオブジェクトを作成する。
+	console.log(native.hello());
+	mainWindow =
+		new BrowserWindow({
+			width: 1280,
+			height: 720,
+			webPreferences: {
+				nodeIntegration: false,
+				nodeIntegrationInWorker: false,
+				contextIsolation: true,
+				preload: path.join(__dirname, "preload.js"),
+				devTools: isDev,
+			},
+		});
 
-    // 読み込む index.html。
-    // tsc でコンパイルするので、出力先の dist の相対パスで指定する。
-    mainWindow.loadFile(__dirname + '/index.html');
+	// 読み込む index.html。
+	// tsc でコンパイルするので、出力先の dist の相対パスで指定する。
+	mainWindow.loadFile(__dirname + "/index.html");
 
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+	mainWindow.on(
+		"closed",
+		() => {
+			mainWindow = null;
+		},
+	);
 
-    if (isDev) {
-        // 開発者ツールを起動する
-        mainWindow.webContents.openDevTools();
-    }
+	if (isDev) {
+		// 開発者ツールを起動する
+		mainWindow.webContents.openDevTools();
+	}
 };
 
 // Electronの起動準備が終わったら、ウィンドウを作成する。
 app.whenReady().then(createWindow);
 
 // すべての ウィンドウ が閉じたときの処理
-app.on('window-all-closed', () => {
-    // macOS 以外では、メインプロセスを停止する
-    // macOS では、ウインドウが閉じてもメインプロセスは停止せず
-    // ドックから再度ウインドウが表示されるようにする。
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
+app.on(
+	"window-all-closed",
+	() => {
+		// macOS 以外では、メインプロセスを停止する
+		// macOS では、ウインドウが閉じてもメインプロセスは停止せず
+		// ドックから再度ウインドウが表示されるようにする。
+		if (process.platform !== "darwin") {
+			app.quit();
+		}
+	},
+);
 
-app.on('activate', () => {
-    // macOS では、ウインドウが閉じてもメインプロセスは停止せず
-    // ドックから再度ウインドウが表示されるようにする。
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
+app.on(
+	"activate",
+	() => {
+		// macOS では、ウインドウが閉じてもメインプロセスは停止せず
+		// ドックから再度ウインドウが表示されるようにする。
+		if (BrowserWindow.getAllWindows().length === 0) {
+			createWindow();
+		}
+	},
+);
 
-ipcMain.on('hello', (event, arg) => {
-    console.log(native.hello());
-});
+ipcMain.on(
+	"hello",
+	(event, arg) => {
+		console.log(native.hello());
+	},
+);
